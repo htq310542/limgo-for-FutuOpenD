14acb023b45913896b5ae484c7aa868590d998e3
aisyer <aisyer@labtop.local>
2018-08-02 21:41:55 +0800

    fix import

--- futupb/gen.sh
+++ futupb/gen.sh
@@ -1,17 +1,3 @@
 
 #!bin/sh
-for file in ./*.proto
-do
-    if test -f $file
-    then
-        new_dir=${file%.*}
-        mkdir $new_dir
-        # echo ${new_dir}
-
-        protoc -I=./ --go_out=./$new_dir/ $file
-    fi
-    if test -d $file
-    then
-        echo $file "is dir"
-    fi
-done
+protoc -I . --go_out=import_path=futupb:. ./*.proto
